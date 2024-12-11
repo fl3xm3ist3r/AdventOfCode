e528f74c5d8c7b6e361c0bd71ada5ff7c9faf4ae
Bryan Schmid <bs@nexplore.ch>
2024-12-11 16:22:09 +0100

    day 7 logic

---
+++ 2024/src/AdventOfCode2024/Days/Day07.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Days;
+
+public class Day07 : DayBase
+{
+    public override ValueTask<string> Solve_1()
+    {
+
+        var x = 32;
+        string comb = Convert.ToString(x, 2).PadLeft(5, '0');
+
+        var total = 0;
+        return new ValueTask<string>(total.ToString());
+    }
+
+   
+
+    //private static (int yOffset, int xOffset) GetDirectionOffset(Direction direction)
+    //{
+
+    //}
+}
