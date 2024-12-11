400e615ece2d797ff2edf3dfa2b23c2afef58526
Fl3xm3ist3r <artopien.net@gmail.com>
2024-12-11 22:54:15 +0100

    refactor day 9

--- 2024/src/AdventOfCode2024/Days/Day09.cs
+++ 2024/src/AdventOfCode2024/Days/Day09.cs
@@ -6,18 +6,17 @@ public class Day09 : DayBase
     {
         var disk = ParseInputToDisk(Input.Value);
 
-        while(true)
+        while (true)
         {
             var dotIndex = disk.IndexOf(".");
             var numberIndex = GetLastIndexOfNot(".", disk);
 
-            if(numberIndex < dotIndex)
+            if (numberIndex < dotIndex)
             {
-                // sorted
-                break;
+                break; // Sorted
             }
 
-            Swap(disk ,dotIndex, numberIndex);
+            Swap(disk, dotIndex, numberIndex);
         }
 
         var total = Checksum(disk);
@@ -27,39 +26,32 @@ public class Day09 : DayBase
     public override ValueTask<string> Solve_2()
     {
         var disk = ParseInputToDisk(Input.Value);
+        int index = disk.Count - 1;
 
-        var index = disk.Count - 1;
-        while (true)
+        while (index >= 0)
         {
-            if(index < 0)
+            for (int dotIndex = 0; dotIndex <= index;)
             {
-                break;
-            }
+                var (dotStart, dotEnd) = GetIndexOfPartAfterIndex(".", disk, dotIndex);
+                var (numStart, numEnd) = GetLastIndexOfNotPartBeforeIndex(".", disk, index);
 
-            var dotIndex = 0;
-            while (true)
-            {
-                var (dotStartIndex, dotEndIndex) = GetIndexOfPartAfterIndex(".", disk, dotIndex);
-                var (numberStartIndex, numberEndIndex) = GetLastIndexOfNotPartBeforeIndex(".", disk, index);
-
-                if(index < dotIndex ||
-                   numberStartIndex < dotStartIndex)
+                if (numStart < 0 || dotStart < 0 || numStart < dotStart)
                 {
-                    // no match found skipping
-                    index = numberStartIndex - 1;
+                    index = numStart - 1;
                     break;
                 }
 
-                var numberCount = numberEndIndex - numberStartIndex + 1;
-                var dotCount = dotEndIndex - dotStartIndex + 1;
-                if (numberCount <= dotCount)
+                int numberLength = numEnd - numStart + 1;
+                int dotLength = dotEnd - dotStart + 1;
+
+                if (numberLength <= dotLength)
                 {
-                    SwapSegments(disk, numberStartIndex, dotStartIndex, numberCount);
-                    index = numberStartIndex - 1;
+                    SwapSegments(disk, numStart, dotStart, numberLength);
+                    index = numStart - 1;
                     break;
                 }
 
-                dotIndex = dotEndIndex + 1;
+                dotIndex = dotEnd + 1;
             }
         }
 
@@ -67,99 +59,64 @@ public class Day09 : DayBase
         return new ValueTask<string>(total.ToString());
     }
 
-    static long Checksum(List<string> disk)
+    private static long Checksum(List<string> disk)
     {
-        long total = 0;
-        for (int i = 0; i < disk.Count; i++)
-        {
-            if (disk[i] != ".")
-            {
-                total += long.Parse(disk[i]) * i;
-            }
-        }
-
-        return total;
+        return disk
+            .Select((value, index) => value == "." ? 0 : long.Parse(value) * index)
+            .Sum();
     }
 
-    static (int startIndex, int endIndex) GetIndexOfPartAfterIndex(string value, List<string> disk, int afterIndex)
+    private static (int startIndex, int endIndex) GetIndexOfPartAfterIndex(string value, List<string> disk, int afterIndex)
     {
-        var startIndex = -1;
-        var endIndex = -1;
+        int startIndex = disk.FindIndex(afterIndex, v => v == value);
 
-        for (int i = afterIndex; i < disk.Count; i++)
+        if (startIndex == -1)
         {
-            if (disk[i] == value && startIndex == -1)
-            {
-                startIndex = i;
-            }
-            if (disk[i] != value && startIndex != -1)
-            {
-                endIndex = i - 1;
-                break;
-            }
+            return (-1, -1);
         }
 
+        int endIndex = disk.FindIndex(startIndex, v => v != value);
+        return endIndex == -1 ? (startIndex, disk.Count - 1) : (startIndex, endIndex - 1);
+    }
+
+    private static (int startIndex, int endIndex) GetLastIndexOfNotPartBeforeIndex(string value, List<string> disk, int beforeIndex)
+    {
+        int endIndex = disk.FindLastIndex(beforeIndex, v => v != value);
+
+        if (endIndex == -1)
+        {
+            return (-1, -1);
+        }
+
+        string matchValue = disk[endIndex];
+        int startIndex = disk.FindLastIndex(endIndex, v => v != matchValue) + 1;
         return (startIndex, endIndex);
     }
 
-    static (int startIndex, int endIndex) GetLastIndexOfNotPartBeforeIndex(string value, List<string> disk, int beforeIndex)
+    private static int GetLastIndexOfNot(string value, List<string> disk)
     {
-        var startIndex = -1;
-        var endIndex = -1;
-
-        string unknown = "";
-        for (int i = beforeIndex; i >= 0; i--)
-        {
-            if (disk[i] != value && unknown == "")
-            {
-                unknown = disk[i];
-                endIndex = i;
-            }
-            if (disk[i] != unknown && unknown != "")
-            {
-                startIndex = i + 1;
-                break;
-            }
-        }
-
-        return (startIndex, endIndex);
+        return disk.FindLastIndex(v => v != value);
     }
 
-    static int GetLastIndexOfNot(string value, List<string> disk)
+    private static void SwapSegments(List<string> list, int start1, int start2, int length)
     {
-        for (int i = disk.Count - 1; i >= 0; i--)
+        for (int i = 0; i < length; i++)
         {
-            if (disk[i] != value)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private static void SwapSegments(List<string> list, int numberStartIndex, int dotStartIndex, int length)
-    {
-        for(int i = 0; i < length; i++)
-        {
-            var test = list[numberStartIndex + i];
-            var test2 = list[dotStartIndex + i];
-            Swap(list, numberStartIndex + i, dotStartIndex + i);
+            Swap(list, start1 + i, start2 + i);
         }
     }
 
     private static void Swap(List<string> list, int index1, int index2)
     {
-        string temp = list[index1];
-        list[index1] = list[index2];
-        list[index2] = temp;
+        (list[index1], list[index2]) = (list[index2], list[index1]);
     }
 
     private static List<string> ParseInputToDisk(string Input)
     {
         var disk = new List<string>();
-
         var isNumber = true;
         var count = 0;
+
         foreach (char value in Input)
         {
             for (int x = 0; x < value - '0'; x++)
